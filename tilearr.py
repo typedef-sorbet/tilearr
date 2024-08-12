d023962bac8d19b3804ed6105b54bb010c000d50
Spencer Warneke <warnespe001@gmail.com>
2024-08-12 15:30:27 -0700

    Resiliency

--- tilearr.py
+++ tilearr.py
@@ -1,101 +1,97 @@
-from sys import argv
+from sys import argv, exit
+from os.path import isfile
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog="tilemap2arr",
-    description="Transforms output of rgbgfx to a .c file that GBDK can use",
+    prog="tilearr",
+    description="Formats output(s) of rgbgfx as a .c file that GBDK can use",
 )
 
-parser.add_argument("-d", "--tiledata", required=True)
-parser.add_argument("-t", "--tilemap", required=True)
-parser.add_argument("-p", "--palette", required=True)
-parser.add_argument("-a", "--attrmap", required=True)
-parser.add_argument("-n", "--name", default="background")
-parser.add_argument("-o", "--output", default="tilemap2arr.c")
+parser.add_argument("-d", "--tiledata", default="", help="Color data for each tile. This should be the default output file of rgbgfx.")
+parser.add_argument("-t", "--tilemap", default="", help="Tile mapping data. This should be the same as what was output for rgbgfx's -t/-T flag.")
+parser.add_argument("-p", "--palette", default="", help="Palette mapping data. This should be the same as what was output for rgbgfx's -p/-P flag.")
+parser.add_argument("-a", "--attrmap", default="", help="Tile mapping data. This should be the same as what was output for rgbgfx's -a/-A flag.")
+parser.add_argument("-n", "--name", default="image", help="What you want each C array to be prefixed with in the resulting .c file.")
+parser.add_argument("-o", "--output", default="image.c", help="Output file, structured as a C source file.")
 
 args = parser.parse_args()
 
-with (open(args.tiledata, mode='rb') as tiledata_file,
-      open(args.tilemap, mode='rb') as tilemap_file,
-      open(args.palette, mode='rb') as palette_file,
-      open(args.attrmap, mode='rb') as attrmap_file,
-      open(args.output, mode='w') as outfile):
-    tilemap_data = tilemap_file.read()
-    tiledata = tiledata_file.read()
-    palette_data = palette_file.read()
-    attrmap_data = attrmap_file.read()
+valid = True
 
+for file, descriptor in [(args.tiledata, "Tile data"), 
+                        (args.tilemap, "Tile map"), 
+                        (args.palette, "Palette"), 
+                        (args.attrmap, "Attribute map")]:
+    if file != "" and not isfile(file):
+        print(f"Error: {descriptor} file {file} does not exist.")
+    valid = valid and (file == "" or isfile(file))
+
+if not valid:
+    exit(1)
+
+with open(args.output, mode='w') as outfile:
     outfile.write("#include <gb/gb.h>\n")
-    outfile.write(f"const UINT16 {args.name}_tileCount = {len(tiledata) // 16};")
 
-    # Tile data
+    # Raw tile data
+    if args.tiledata and isfile(args.tiledata):
+        with open(args.tiledata, mode='rb') as tiledata_file:
+            tiledata = tiledata_file.read()
 
-    outfile.write(f"const unsigned char {args.name}_tileData[] = {{\n")
-    for tile_num in range(len(tiledata) // 16):
-        offset = tile_num * 16
-        this_tile = tiledata[offset:offset+16]
-        tiles_as_hex = ",".join(
-            map(lambda _tile: "0x{:02X}".format(_tile), this_tile)
-        )
-        outfile.write(f"/* Tile {tile_num} */ {tiles_as_hex},\n")
-    outfile.write("};\n")
+        outfile.write(f"const UINT16 {args.name}_tileCount = {len(tiledata) // 16};\n")
+        outfile.write(f"const unsigned char {args.name}_tileData[] = {{\n")
+        for tile_num in range(len(tiledata) // 16):
+            offset = tile_num * 16
+            this_tile = tiledata[offset:offset+16]
+            tiles_as_hex = ",".join(
+                map(lambda _tile: "0x{:02X}".format(_tile), this_tile)
+            )
+            outfile.write(f"/* Tile {tile_num} */ {tiles_as_hex},\n")
+        outfile.write("};\n")
 
-    # Tilemap
+    # Tilemap (mapping each tile to an 8x8 space on the screen)
+    if args.tilemap and isfile(args.tilemap):
+        with open(args.tilemap, mode='rb') as tilemap_file:
+            tilemap_data = tilemap_file.read()
 
-    outfile.write(f"const unsigned char {args.name}_tileMapData[] = {{\n")
+        outfile.write(f"const unsigned char {args.name}_tileMapData[] = {{\n")
 
-    for tilemap_num in range(len(tilemap_data) // 16):
-        offset = tilemap_num * 16
-        this_mapping = tilemap_data[offset : offset + 16]
-        mapping_as_hex = ",".join(
-            map(lambda t: "0x{:02X}".format(t), this_mapping)
-        )
-        outfile.write(f"/* Mapping {tilemap_num} */ {mapping_as_hex},\n")
-    outfile.write("};\n")
+        for tilemap_num in range(len(tilemap_data) // 16):
+            offset = tilemap_num * 16
+            this_mapping = tilemap_data[offset : offset + 16]
+            mapping_as_hex = ",".join(
+                map(lambda t: "0x{:02X}".format(t), this_mapping)
+            )
+            outfile.write(f"\t{mapping_as_hex},\n")
+        outfile.write("};\n")
 
-    # Attrmap
+    # Attrmap (assigning attributes to each 8x8 space on the screen, e.g. flip, palette index, etc.)
+    if args.attrmap and isfile(args.attrmap):
+        with open(args.attrmap, mode='rb') as attrmap_file:
+            attrmap_data = attrmap_file.read()
 
-    outfile.write(f"const unsigned char {args.name}_attrMapData[] = {{\n")
+        outfile.write(f"const unsigned char {args.name}_attrMapData[] = {{\n")
 
-    for attr_num in range(len(attrmap_data) // 16):
-        offset = attr_num * 16
-        this_attr = attrmap_data[offset : offset + 16]
-        attr_as_hex = ",".join(
-            map(lambda a: "0x{:02X}".format(a), this_attr)
-        )
-        outfile.write(f"/* Attr {attr_num} */ {attr_as_hex},\n")
-    outfile.write("};\n")
+        for attr_num in range(len(attrmap_data) // 16):
+            offset = attr_num * 16
+            this_attr = attrmap_data[offset : offset + 16]
+            attr_as_hex = ",".join(
+                map(lambda a: "0x{:02X}".format(a), this_attr)
+            )
+            outfile.write(f"\t{attr_as_hex},\n")
+        outfile.write("};\n")
 
     # Palette
+    if args.palette and isfile(args.palette):
+        with open(args.palette, mode='rb') as palette_file:
+            palette_data = palette_file.read()
 
-    print(f"palette_data: {palette_data} (len {len(palette_data)})")
+        outfile.write(f"const UWORD {args.name}_palette[{len(palette_data) // 2}] = {{\n")
 
-    outfile.write(f"const UWORD {args.name}_palette[{len(palette_data) // 2}] = {{\n")
-
-    # This one is a little different.
-    # Pull out palette data 2 bytes at a time, and swap the endianness.
-
-    # Palette file ends in 0x0A, strip that off
-    for palette_num in range(len(palette_data) // 2):
-        offset = palette_num * 2
-        this_palette = palette_data[offset:offset+2]
-        palette_endian_swapped = "0x{:02X}{:02X}".format(this_palette[1], this_palette[0])
-        outfile.write(palette_endian_swapped + ",\n")
-    outfile.write("};\n")
-
-# Original
-
-# with open(argv[1], mode="rb") as tilemap_file:
-#     with open(argv[1] + ".t2aout", mode="w") as outfile:
-#         tilemap_data = tilemap_file.read()
-#         outfile.write("#include <gb/gb.h>")
-#         outfile.write(f"const UINT16 tileCount = {len(tilemap_data)};\n")
-#         outfile.write("const unsigned char data = {\n")
-#         for tile_num in range(len(tilemap_data) // 16):
-#             offset = tile_num * 16
-#             tile_data = tilemap_data[offset : offset + 16]
-#             tiles_as_hex = ",".join(
-#                 map(lambda _tile: "0x{:02X}".format(_tile), tile_data)
-#             )
-#             outfile.write(f"/* Tile {tile_num} */ {tiles_as_hex},\n")
-#         outfile.write("};\n")
+        # This one is a little different.
+        # Pull out palette data 2 bytes at a time, and swap the endianness.
+        for palette_num in range(len(palette_data) // 2):
+            offset = palette_num * 2
+            this_palette = palette_data[offset:offset+2]
+            palette_endian_swapped = "0x{:02X}{:02X}".format(this_palette[1], this_palette[0])
+            outfile.write(palette_endian_swapped + ",\n")
+        outfile.write("};\n")
